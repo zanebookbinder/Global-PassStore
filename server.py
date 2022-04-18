3aa856d5dc2a1ea61f4dc13c4f54feba86c22d87
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-18 13:15:00 -0400

    added starter code

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
+import xmlrpc.client, sys
+
+
 server
+
+RPC methods:
+1. Save password
+	-update map
+2. Retrieve password
+
+
+hostname = sys.argv[1] # ip address of this server?
+print(hostname)
+
+server = xmlrpc.client.Server(hostname)
+
+answer = server.FrontEndServer.search('romance')
