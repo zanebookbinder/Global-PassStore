98972d0164f0dc14ff33a47aaa5b2ae73ef5c2be
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-18 17:30:07 -0400

    made two servers

--- client.py
+++ client.py
@@ -1,15 +1,21 @@
 import xmlrpc.client
 
-#client
+s1 = xmlrpc.client.ServerProxy('http://localhost:8000')
+s2 = xmlrpc.client.ServerProxy('http://localhost:8001')
+
+print(s1.register('zbookbin', 'zbookbin nike.com', 'hello1234'))
+print(s1.register('dlittle', 'dlittle amazon.com', 'uhoh789'))
+
+print(s2.register('sbarker', 'sbarker gymshark.com', 'compscifun'))
+print(s1.register('zbookbin', 'zbookbin google.com', 'secondpass5'))
+print(s2.register('ahameed', 'ahameed bowdoin.edu', 'oops5678'))
+
+print(s1.p())
+print(s2.p())
+
+# print(s1.search(2))
+# print(s1.search(3))
 
 
-s = xmlrpc.client.ServerProxy('http://localhost:8001')
-print(s.register(1, 35))
-print(s.register(2, 27))
-
-print(s.search(2))
-print(s.search(3))
-
-
-# Print list of available methods
-print(s.system.listMethods())
+# # Print list of available methods
+# print(s1.system.listMethods())
--- server.py
+++ server.py
@@ -4,31 +4,75 @@ from xmlrpc.server import SimpleXMLRPCRequestHandler
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
 
-# server
+passmap = {}
+storemap = {}
 
-# RPC methods:
-# 1. Save password
-# 	-update map
-# 2. Retrieve password
+if sys.argv[1] == '8000':
+	host = 'http://localhost:8001'
+else:
+	host = 'http://localhost:8000'
 
-m = {}
+s = xmlrpc.client.ServerProxy(host)
+myname = 'http://localhost:' + str(sys.argv[1])
 
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
+# class RequestHandler(SimpleXMLRPCRequestHandler):
+#     rpc_paths = ('/RPC2',)
 
-with SimpleXMLRPCServer(('localhost', 8001), requestHandler=RequestHandler, allow_none=True) as server:
+#in with line: requestHandler=RequestHandler
+
+with SimpleXMLRPCServer(('localhost', int(sys.argv[1])), allow_none=True) as server:
 	
 	server.register_introspection_functions()
 
-	def register(key, val):
-		m[key] = val
+	def register(username, key, val):
+		user = key.split(' ')[0]
+
+		if username != user:
+			return 'no permissions'
+
+		site = key.split(' ')[1]
+
+		chunk1 = val[0:len(val)//2]
+		chunk2 = val[len(val)//2+1:]
+
+		put(key + '1', chunk1)
+		s.put(key + '2', chunk2)
+
+		propogate(key, myname)
+		propogate(key, host)
+
+		return 1
+
+	def put(key, val):
+		passmap[key] = val
 		return 1
 
 	def search(key):
-		if key in m:
-			return m[key]
+		if key in passmap:
+			return passmap[key]
 		return -1
+
+	def p():
+		#return str(passmap), str(storemap)
+		return str(storemap)
+
+	def addHost(user, host):
+		if user in storemap:
+			if host not in storemap[user]:
+				storemap[user].append(host) # machine that password is stored on
+		else:
+			storemap[user] = [host]
+
+	def propogate(user, host):
+		addHost(user, host)
+		s.addHost(user, host)
+			
+
 
 	server.register_function(register)
 	server.register_function(search)
+	server.register_function(put)
+	server.register_function(p)
+	server.register_function(addHost)
+	server.register_function(propogate)
 	server.serve_forever()
