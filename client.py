aad14896537a0040c61efb49798fc3e8cac94e97
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-19 17:06:40 -0400

    working password retrieval

--- client.py
+++ client.py
@@ -3,18 +3,19 @@ import xmlrpc.client
 s1 = xmlrpc.client.ServerProxy('http://localhost:8000')
 s2 = xmlrpc.client.ServerProxy('http://localhost:8001')
 
-print(s1.register('zbookbin', 'zbookbin nike.com', 'hello1234'))
-print(s1.register('dlittle', 'dlittle amazon.com', 'uhoh789'))
+s1.register('zbookbin', 'zbookbin nike.com', 'hello1234')
+s1.register('dlittle', 'dlittle amazon.com', 'uhoh789')
 
-print(s2.register('sbarker', 'sbarker gymshark.com', 'compscifun'))
-print(s1.register('zbookbin', 'zbookbin google.com', 'secondpass5'))
-print(s2.register('ahameed', 'ahameed bowdoin.edu', 'oops5678'))
+s2.register('sbarker', 'sbarker gymshark.com', 'compscifun')
+s1.register('zbookbin', 'zbookbin google.com', 'secondpass5')
+s2.register('ahameed', 'ahameed bowdoin.edu', 'oops5678')
 
-print(s1.p())
-print(s2.p())
+print(s1.getStoreMap())
+print(s2.getStoreMap())
 
-# print(s1.search(2))
-# print(s1.search(3))
+print(s1.search('zbookbin google.com'))
+print(s1.search('zbookbin amazon.com'))
+
 
 
 # # Print list of available methods
--- server.py
+++ server.py
@@ -8,11 +8,11 @@ passmap = {}
 storemap = {}
 
 if sys.argv[1] == '8000':
-	host = 'http://localhost:8001'
+	otherHost = 'http://localhost:8001'
 else:
-	host = 'http://localhost:8000'
+	otherHost = 'http://localhost:8000'
 
-s = xmlrpc.client.ServerProxy(host)
+s = xmlrpc.client.ServerProxy(otherHost)
 myname = 'http://localhost:' + str(sys.argv[1])
 
 # class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -24,6 +24,8 @@ with SimpleXMLRPCServer(('localhost', int(sys.argv[1])), allow_none=True) as ser
 	
 	server.register_introspection_functions()
 
+	# registers a username (zbookbin), key (zbookbin amazon.com), value (password)
+	# across this machine and the other machine
 	def register(username, key, val):
 		user = key.split(' ')[0]
 
@@ -32,47 +34,95 @@ with SimpleXMLRPCServer(('localhost', int(sys.argv[1])), allow_none=True) as ser
 
 		site = key.split(' ')[1]
 
-		chunk1 = val[0:len(val)//2]
-		chunk2 = val[len(val)//2+1:]
+		chunk1 = val[0:len(val)//2] # password pieces
+		chunk2 = val[len(val)//2:]
 
-		put(key + '1', chunk1)
-		s.put(key + '2', chunk2)
+		put(key + '1', chunk1) # store chunk1 on this machine
+		s.put(key + '2', chunk2) # store chunk2 on the other machine
 
-		propogate(key, myname)
-		propogate(key, host)
+		propogate(key, myname, 1) # tell other host that this machines stores a piece of the zbookin amazon.com entry
+		propogate(key, otherHost, 2)  # tell other host that it stores a piece of the zbookin amazon.com entry
 
 		return 1
 
+	# put a (user + site), (password) pair into memory
 	def put(key, val):
 		passmap[key] = val
 		return 1
 
-	def search(key):
+	# return a password if stored (given a user + site)
+	def lookup(key):
 		if key in passmap:
 			return passmap[key]
+
 		return -1
 
-	def p():
-		#return str(passmap), str(storemap)
+	# collect the pieces of a password given user + site
+	def search(key):
+
+		if key not in storemap:
+			return 'No record of key'
+
+		machines = storemap[key]
+		pieces = ['' for i in range(2)]
+
+		for piece_num in machines:
+			if machines[piece_num] == myname:
+				pieces[piece_num-1] = lookup(key + str(piece_num))
+			else:
+				pieces[piece_num-1] = s.lookup(key + str(piece_num))
+
+		return ''.join(pieces)
+
+		
+		
+		# results = {}
+		# for i in range(1,3): # how many pieces the password is in
+		# 	newKey = key + str(i)
+			
+		# 	res = lookup(newKey) # check for this piece on this machine
+
+		# 	if res != -1:
+		# 		results[i] = res
+
+		# 	res2 = s.lookup(newKey)
+
+		# 	if res2 != -1:
+		# 		results[i] = res2
+
+		finalPassword = ''
+		for i in range(1,3):
+			if i not in results:
+				return -1
+			finalPassword += results[i]
+
+		return finalPassword
+
+	def getStoreMap():
 		return str(storemap)
 
-	def addHost(user, host):
-		if user in storemap:
-			if host not in storemap[user]:
-				storemap[user].append(host) # machine that password is stored on
-		else:
-			storemap[user] = [host]
+	def getPassMap():
+		return str(passmap)
 
-	def propogate(user, host):
-		addHost(user, host)
-		s.addHost(user, host)
+	def addHost(user, host, piece_num):
+		if user in storemap:
+			if piece_num not in storemap[user]:
+				storemap[user][piece_num] = host # machine that password chunk is stored on
+		else:
+			storemap[user] = {piece_num: host}
+
+	def propogate(user, host, piece_num):
+		addHost(user, host, piece_num)
+		s.addHost(user, host, piece_num)
 			
 
 
 	server.register_function(register)
 	server.register_function(search)
 	server.register_function(put)
-	server.register_function(p)
+	server.register_function(getStoreMap)
+	server.register_function(getPassMap)
 	server.register_function(addHost)
 	server.register_function(propogate)
+	server.register_function(lookup)
 	server.serve_forever()
