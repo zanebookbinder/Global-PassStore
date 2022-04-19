90fbf8392967f879bb47db911673364a33a79ed4
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-19 19:38:59 -0400

    did execute_python.sh

--- execute_python.sh
+++ execute_python.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#!/bin/bash
+HOSTS="52.90.4.149 54.236.244.145 54.211.164.149 54.205.63.8"
 
-HOSTS="host1 host2 host3"
-SCRIPT="pwd; ls"
-
-USERNAME=$1
-
+KEYPAIR_PATH=$1
+USERNAME=$2
 
 for HOSTNAME in ${HOSTS} ; do
-    ssh -i ${USERNAME}-keypair ${USERNAME}@${HOSTNAME} "${SCRIPT}"
+    ssh -i ${KEYPAIR_PATH} ${USERNAME}@${HOSTNAME} "cd project-4---final-project-zane-danny-ahmed && eval \`ssh-agent\` && ssh-add ~/.ssh/${USERNAME}-keypair && git pull"
 done
