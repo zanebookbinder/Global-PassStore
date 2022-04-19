7174a221c2487498b17c8f346b329bd3b49ee910
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-19 19:33:34 -0400

    added argument to clone_repo

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,8 +2,9 @@
 
 HOSTS="52.90.4.149 54.236.244.145 54.211.164.149 54.205.63.8"
 
-USERNAME=$1
+KEYPAIR_PATH=$1
+USERNAME=$2
 
 for HOSTNAME in ${HOSTS} ; do
-    ssh -i ${USERNAME}-keypair ${USERNAME}@${HOSTNAME} "eval \`ssh-agent\` && ssh-add .ssh/${USERNAME}-keypair && git clone git@github.com:bowdoin-dsys/project-4---final-project-zane-danny-ahmed.git"
+    ssh -i ${KEYPAIR_PATH} ${USERNAME}@${HOSTNAME} "eval \`ssh-agent\` && ssh-add .ssh/${USERNAME}-keypair && git clone git@github.com:bowdoin-dsys/project-4---final-project-zane-danny-ahmed.git"
 done
