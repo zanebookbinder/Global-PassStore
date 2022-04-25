872be49b3f94b0e249df0e96fb734f4f13692e83
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-25 14:01:07 -0400

    oops

--- server.py
+++ server.py
@@ -196,7 +196,7 @@ try:
 					else:
 						print(f'expected pieceNum {pieceNum} on {hostAddr} but no password piece was found!!!')
 
-			return '|' + ''.join(pieces.strip()) + '|'
+			return '|' + ''.join(pieces).strip() + '|'
 
 			finalPassword = ''
 			for i in range(1,3):
