63ec413bba3137002406b7c597627c43ad4e0853
Ahmed Hameed <ahameed@bowdoin.edu>
2022-04-27 18:48:25 -0400

    cloning repo on all server machines

--- clone_repo.sh
+++ clone_repo.sh
@@ -14,5 +14,8 @@ HOSTS="35.172.235.46 44.199.229.51 3.22.185.101
 KEYPAIR_PATH=$1
 
 for HOSTNAME in ${HOSTS} ; do
+    # scp [options] [source]:file [dest]:file
+    scp -i ${KEYPAIR_PATH}/$USER-keypair ${KEYPAIR_PATH}/$USER-keypair $USER@${HOSTNAME}:${KEYPAIR_PATH}/$USER-keypair 
+    # scp -i ${KEYPAIR_PATH}/$USER-keypair ${KEYPAIR_PATH}/$USER-keypair $USER@${HOSTNAME}:${KEYPAIR_PATH}/$USER-keypair 
     ssh -i ${KEYPAIR_PATH}/$USER-keypair $USER@${HOSTNAME} "rm -rf project-4---final-project-zane-danny-ahmed && echo 'Host github.com' > .ssh/config && echo '    IdentityFile ~/.ssh/$USER-keypair' >> .ssh/config && echo '    StrictHostKeyChecking no' >> .ssh/config && chmod 600 .ssh/config && git clone git@github.com:bowdoin-dsys/project-4---final-project-zane-danny-ahmed.git"
 done
