b337237357329f118a534c12148169a907d57180
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-28 15:41:59 -0400

    type 3

--- server.py
+++ server.py
@@ -57,7 +57,9 @@ try:
 
 	# hostmap = {'52.90.4.149':'172.31.55.0', '54.236.244.145':'172.31.53.196', '54.211.164.149':'172.31.52.8', '54.205.63.8':'172.31.53.249'}
 	# privateIPs = [ '172.31.55.0', '172.31.53.196', '172.31.52.8', '172.31.53.249']
-	myPublicIP = os.popen('curl -s ifconfig.me').readline()
+	myPublicIP = os.popen('curl -s ifconfig.me')
+	print('Type1:', type(myPublicIP))
+	myPublicIP = myPublicIP.readline()
 	mytestIP = '0.0.0.0'
 
 	# res = os.popen('ifconfig | grep inet | head -n 1').readline()
