b6b6a99871ab855af973119632f84c74c2ddf9a9
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-04-27 20:39:55 -0400

    try 2

--- dummyClient.py
+++ dummyClient.py
@@ -1,6 +1,6 @@
 import xmlrpc.client
 
-s = xmlrpc.client.ServerProxy('http://172.31.70.66:8013')
+s = xmlrpc.client.ServerProxy('http://35.172.235.46:8013')
 print(s.add(2,3))
 
 print(s.system.listMethods())
