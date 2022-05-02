f907f65c02ffeebfa4cd865c64dab9f15de41ac4
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-05-01 21:13:13 -0400

    working on threading

--- server.py
+++ server.py
@@ -34,6 +34,7 @@ import sys
 import math
 import random
 import time
+from theading import Thread
 
 hosts = ['35.172.235.46', '44.199.229.51', '3.22.185.101', '18.191.134.62', '13.57.194.105', 
 '54.177.19.64', '34.222.143.244', '54.202.50.11', '13.245.182.179', '13.246.6.180', '18.166.176.112', 
@@ -43,6 +44,12 @@ hosts = ['35.172.235.46', '44.199.229.51', '3.22.185.101', '18.191.134.62', '13.
 '3.73.75.196', '34.244.200.204', '3.250.224.218', '18.130.129.70', '13.40.95.197', '15.160.192.179',
 '15.160.153.56', '35.180.109.137', '35.180.39.12', '13.48.137.111', '13.48.3.201', '15.185.175.128', 
 '157.175.185.52', '15.228.252.96', '15.229.0.10']
+
+hosts0 = hosts[:10]
+hosts1 = hosts[10:20]
+hosts2 = hosts[20:30]
+hosts3 = hosts[30:]
+
 otherHosts = hosts.copy()
 
 localPasswordData = {}
@@ -247,8 +254,18 @@ def propagate(user, chunkStorageList):
 	addHosts(user, chunkStorageList)
 
 	print("sending update to other server nodes")
-	for ip, connection in otherServers.items():
-		connection.addHosts(user, chunkStorageList)
+	threads = []
+	for i in range(4):
+		threads
+	thread0 = Thread(target = propogateThread, args = (user, hosts0, chunkStorageList))
+	
+    thread.start()
+    thread.join()
+
+def propogateThread(user, hostsList, chunkStorageList):
+	for ip in hostsList:
+		otherServers[ip].addHost(user, chunkStorageList)
+
 
 
 def getPrivateIP():
