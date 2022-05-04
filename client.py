3a323a746ba0b7be5298dc6e6dd94d5e9a42f6c4
Zane Bookbinder <zbookbin@bowdoin.edu>
2022-05-04 12:58:52 -0400

    updated client

--- client.py
+++ client.py
@@ -12,28 +12,31 @@ def main():
 	user = input("Please enter your username to login: ")
 
 	print("Thanks for logging in! Your username is " + user)
-	print("Executable commands:\n  ->register [url] [password]\n  ->search [url]\n  ->update [url] [password]\n  ->delete [url]\n")
+	print("Executable commands:\n  ->register/r [url] [password]\n  ->search/s   [url]\n  ->update/u   [url] [password]\n  ->delete/d   [url]\n")
 
 	while(True):
 		parse = input("Enter your command: ").split(' ')
+		if len(parse) == 1 and parse[0] == '':
+			continue
 		if len(parse) < 2:
 			print("Must include correct arguments starting with 'register', 'search', or 'update\'")
 			continue
 		command = parse[0]
 		url = parse[1]
-		if command == 'search':
+		if command == 'search' or command == 's':
 			print('Password for ' + url + ' is: ' + search(user, url) + '\n')
-		elif command == 'delete':
-			delete(user, url)
-		elif command == 'register':
+		elif command == 'delete' or command == 'd':
+			print("One second while we delete your password chunks stored around the globe...")
+			print(delete(user, url) + '\n')
+		elif command == 'register' or command == 'r':
 			if len(parse) < 3:
 				print("Must include three arguments for a register operation")
 
 			print("One second while we register your password around the globe...")
 			password = parse[2]
 			result = register(user, url, password)
-			print(result)
-		elif command == 'update':
+			print(result + '\n')
+		elif command == 'update' or command == 'u':
 			if len(parse) < 3:
 				print("Must include three arguments for a register operation")
 			print("One second while we update your password...")
@@ -41,8 +44,9 @@ def main():
 			result = update(user, url, password)
 			print(result)
 
-
 def register(user, url, password):
+	if len(password) < 4:
+		return "Sorry! Passwords must be at least 4 characters long"
 	userUrl = user + ' ' + url
 	storedLocations = connection.register(user, userUrl, password)
 	if type(storedLocations) == list:
@@ -65,7 +69,7 @@ def update(user, url, password):
 
 def delete(user, url):
 	userUrl = user + ' ' + url
-	connection.delete(user, userUrl)
+	return connection.delete(user, userUrl)
 
 if __name__ == "__main__":
 	main()
